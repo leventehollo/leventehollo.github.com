d59d147adfe95bcc5f705fa0b550f5fc640d6f7a
Levente Holló <hollo.levente@gmail.com>
2013-04-07 14:56:21 +0200

    Added snapshot 'playsms' to repository

--- scripts/deploy_snapshot.sh
+++ scripts/deploy_snapshot.sh
@@ -1,7 +1,4 @@
 cp -rv ${HOME}/progz/play-2.1.0/repository/local/cc.hollo ../releases
-cp -rv ${HOME}/progz/play-2.1.0/repository/local/cc.hollo ../releases
-cp -rv ${HOME}/progz/play-2.1.0/repository/local/cc.hollo ../releases
-cp -rv ${HOME}/progz/play-2.1.0/repository/local/cc.hollo ../releases
 cd ..
 git add .
 git commit -m "Added snapshot 'playsms' to repository"
